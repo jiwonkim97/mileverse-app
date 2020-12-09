63129b8809e7daf7696b4f5aabc2c5257004ec1c
jinkook <jinqook0126@naver.com>
2020-12-09 12:40:17 +0900

    add scroll view

--- src/screen/Change.js
+++ src/screen/Change.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Image,View,SafeAreaView,StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Image,View,SafeAreaView,StyleSheet, TouchableWithoutFeedback,ScrollView } from 'react-native';
 import { RegularText, ExtraBoldText, BoldText } from '../components/customComponents';
 import CommonStatusbar from '../components/CommonStatusbar';
 
@@ -22,63 +22,74 @@ const ChangeScreen = (props) =>{
     return (
         <>
             <CommonStatusbar backgroundColor="#F9F9F9"/>
-            <SafeAreaView>
+            <SafeAreaView style={{flex:1}}>
                 <View style={[styles.shadow,styles.header]}>
                     <ExtraBoldText text="마일리지 교환" customStyle={{fontSize:16}}/>
                 </View>
-                <View style={{marginVertical:6,backgroundColor:"#394054",width:"100%",padding:24}}>
-                    <BoldText text={"[필수공지]"} customStyle={styles.bannerTitle}/>
-                    <RegularText text={"본 상품은 구매 후 환불이 불가능합니다."} customStyle={styles.bannerText}/>
-                    <BoldText text={"[이용안내]"} customStyle={[styles.bannerTitle,{marginTop:20}]}/>
-                    <RegularText text={"구매한 MVP는 앱에서 이용이 가능합니다."} customStyle={styles.bannerText}/>
-                    <RegularText text={"MVP는 1원의 가치를 지니고 있습니다."} customStyle={styles.bannerText}/>
-                </View>
-                <View style={{backgroundColor:"#FFFFFF",paddingHorizontal:16,paddingTop:26,height:"100%"}}>
-                    <BoldText text={"MVP 상품권 구매"} customStyle={styles.itemTitle}/>
-                    <View style={{marginTop:16,flexDirection:'row'}}>
-                        <TouchableWithoutFeedback onPress={()=>doBuyCard("M10")}>
-                            <View style={[styles.cardWrap,styles.shadow]}>
-                                <View style={styles.cardImgWrap}>
-                                    <Image source={require("../../assets/img/mvp_gift_10.png")} style={styles.cardImg}/>
-                                </View>
-                                <View style={styles.cardTextWrap}>
-                                    <RegularText text={"MVP 1만원권"} customStyle={styles.salesTitle}/>
-                                    <View style={styles.salesWrap}>
-                                        <ExtraBoldText text={"10%"} customStyle={styles.sales}/>
-                                        <ExtraBoldText text={"9,000"} customStyle={styles.salesPrice}/>
-                                        <RegularText text={"원"} customStyle={{fontSize:13}}/>
-                                        {/* <BoldText text={"10,000원"} customStyle={styles.price}/> */}
-                                    </View>
-                                </View>
-                            </View>
-                        </TouchableWithoutFeedback>
-                        <View style={{width:16}} />
-                        <TouchableWithoutFeedback onPress={()=>doBuyCard("M05")}>
-                            <View style={[styles.cardWrap,styles.shadow]}>
-                                <View style={styles.cardImgWrap}>
-                                    <Image source={require("../../assets/img/mvp_gift_05.png")} style={styles.cardImg}/>
-                                </View>
-                                <View style={styles.cardTextWrap}>
-                                    <RegularText text={"MVP 5천원권"} customStyle={styles.salesTitle}/>
-                                    <View style={styles.salesWrap}>
-                                        <ExtraBoldText text={"5%"} customStyle={styles.sales}/>
-                                        <ExtraBoldText text={"4,750"} customStyle={styles.salesPrice}/>
-                                        <RegularText text={"원"} customStyle={{fontSize:13}}/>
-                                        {/* <BoldText text={"5,000원"} customStyle={styles.price}/> */}
-                                    </View>
-                                </View>
-                            </View>
-                        </TouchableWithoutFeedback>
+                <ScrollView style={{flex:1}}>
+                    <View style={{backgroundColor:"#394054",padding:24,marginVertical:6}}>
+                        <BoldText text={"[필수공지]"} customStyle={styles.bannerTitle}/>
+                        <RegularText text={"본 상품은 구매 후 환불이 불가능합니다."} customStyle={styles.bannerText}/>
+                        <BoldText text={"[이용안내]"} customStyle={[styles.bannerTitle,{marginTop:20}]}/>
+                        <RegularText text={"구매한 MVP는 앱에서 이용이 가능합니다."} customStyle={styles.bannerText}/>
+                        <RegularText text={"MVP는 1원의 가치를 지니고 있습니다."} customStyle={styles.bannerText}/>
                     </View>
-                    <BoldText text={"MVP 교환"} customStyle={[styles.itemTitle,{marginTop:26}]}/>
-                    <View style={{marginTop:16,borderRadius:10,justifyContent:"center",alignItems:"center",overflow:"hidden"}}>
-                        <View style={{marginVertical:13}}>
-                            <Image source={require("../../assets/img/logo_healthPick.png")} />
+                    <View style={{backgroundColor:"#FFFFFF",paddingHorizontal:16,paddingTop:26}}>
+                        <BoldText text={"MVP 상품권 구매"} customStyle={styles.itemTitle}/>
+                        <View style={{marginTop:16,flexDirection:'row'}}>
+                            <TouchableWithoutFeedback onPress={()=>doBuyCard("M10")}>
+                                <View style={[styles.cardWrap,styles.shadow]}>
+                                    <View style={styles.cardImgWrap}>
+                                        <Image source={require("../../assets/img/mvp_gift_10.png")} style={styles.cardImg}/>
+                                    </View>
+                                    <View style={styles.cardTextWrap}>
+                                        <RegularText text={"MVP 1만원권"} customStyle={styles.salesTitle}/>
+                                        <View style={styles.salesWrap}>
+                                            <ExtraBoldText text={"10%"} customStyle={styles.sales}/>
+                                            <ExtraBoldText text={"9,000"} customStyle={styles.salesPrice}/>
+                                            <RegularText text={"원"} customStyle={{fontSize:13}}/>
+                                        </View>
+                                    </View>
+                                </View>
+                            </TouchableWithoutFeedback>
+                            <View style={{width:16}} />
+                            <TouchableWithoutFeedback onPress={()=>doBuyCard("M05")}>
+                                <View style={[styles.cardWrap,styles.shadow]}>
+                                    <View style={styles.cardImgWrap}>
+                                        <Image source={require("../../assets/img/mvp_gift_05.png")} style={styles.cardImg}/>
+                                    </View>
+                                    <View style={styles.cardTextWrap}>
+                                        <RegularText text={"MVP 5천원권"} customStyle={styles.salesTitle}/>
+                                        <View style={styles.salesWrap}>
+                                            <ExtraBoldText text={"5%"} customStyle={styles.sales}/>
+                                            <ExtraBoldText text={"4,750"} customStyle={styles.salesPrice}/>
+                                            <RegularText text={"원"} customStyle={{fontSize:13}}/>
+                                        </View>
+                                    </View>
+                                </View>
+                            </TouchableWithoutFeedback>
                         </View>
-                        <View style={styles.mask} />
-                        <ExtraBoldText text={"Coming soon"} customStyle={{fontSize:20,color:"#FFFFFF",position:"absolute"}}/>
+                        <View style={{marginVertical:26}}>
+                            <BoldText text={"MVP 교환"} customStyle={[styles.itemTitle]}/>
+                            <View style={{marginTop:16,borderRadius:10,justifyContent:"center",alignItems:"center",overflow:"hidden"}}>
+                                <View style={{marginVertical:13}}>
+                                    <Image source={require("../../assets/img/logo_healthPick.png")} />
+                                </View>
+                                <View style={styles.mask} />
+                                <ExtraBoldText text={"Coming soon"} customStyle={{fontSize:20,color:"#FFFFFF",position:"absolute"}}/>
+                            </View>
+                            <View style={{marginTop:16,borderRadius:10,justifyContent:"center",alignItems:"center",overflow:"hidden"}}>
+                                <View style={{marginVertical:13}}>
+                                    <Image source={require("../../assets/img/logo_healthPick.png")} />
+                                </View>
+                                <View style={styles.mask} />
+                                <ExtraBoldText text={"Coming soon"} customStyle={{fontSize:20,color:"#FFFFFF",position:"absolute"}}/>
+                            </View>
+                        </View>
+                        
                     </View>
-                </View>
+                </ScrollView>
+                
             </SafeAreaView>
         </>
     )
